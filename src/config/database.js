bec9649512f200299c4bb108d29e575879de96f8
realeliakim <eliakim@next4.com.br>
2021-08-17 19:38:56 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,10 @@
-
 module.exports = {
-  dialect: 'mysql',
-  host: '127.0.0.1',
-  port: '3306',
-  username: 'root',
-  password: 'mamba',
-  database: 'teste_3lm',
+  dialect: "mysql",
+  host: "127.0.0.1",
+  port: "3306",
+  username: "",
+  password: "",
+  database: "",
   define: {
     timestamps: true,
     underscored: true,
